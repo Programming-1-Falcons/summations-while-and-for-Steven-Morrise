4083daadd5739c4850275ee17f36ba9102714111
Steven-Morrise <morriste007@alpinesd.org>
2024-10-22 11:17:54 -0600

    Update while_summation.py
    While

--- while_summation.py
+++ while_summation.py
@@ -1,1 +1,9 @@
-#while summation code following directions from Readme
+num = int(input("Input a number"))
+x = 0
+t = 0
+a = 0
+while a < num :
+    t = t + 1
+    x = x + t
+    a = a + 1
+print(x)
